bf301689d3cbf36952892b4c9e897aee1f73bfd6
cihatturgut <cihatturgut@yaani.com>
2024-11-10 04:49:22 +0300

    Migration operations and abstract interfaces were prepared.

---
+++ CEK301.DataAccessLayer/Abstract/IProductDal.cs
@@ -0,0 +1,13 @@
+﻿using CEK301.EntityLayer.concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEK301.DataAccessLayer.Abstract
+{
+    public interface IProductDal : IGenericDal<Product>
+    {
+    }
+}
--- CEK301.EntityLayer/concrete/Customer.cs
+++ CEK301.EntityLayer/concrete/Customer.cs
@@ -14,6 +14,8 @@ namespace CEK301.EntityLayer.concrete
         public string CustomerDistrict { get; set; }
         public string CustomerCity { get; set; }
         public List<Order> Orders { get; set; }
+        public bool CustomerStatus { get; set; }
+
 
     }
 }
