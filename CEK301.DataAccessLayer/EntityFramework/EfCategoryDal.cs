3d89a3a071c5fc8e668c6719c2e6fe4defdc4cf8
cihatturgut <cihatturgut@yaani.com>
2024-11-27 05:55:08 +0300

    CEK301|26.11.2024_Completed DataAccessLayer

---
+++ CEK301.DataAccessLayer/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,16 @@
+﻿using CEK301.DataAccessLayer.Abstract;
+using CEK301.DataAccessLayer.Repositories;
+using CEK301.EntityLayer.concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEK301.DataAccessLayer.EntityFramework
+{
+    public class EfCategoryDal : GenericRepository<Category>, ICategoryDal
+    {
+
+    }
+}
